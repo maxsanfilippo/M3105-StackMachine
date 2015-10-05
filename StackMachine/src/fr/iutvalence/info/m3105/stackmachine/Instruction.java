d3344ea58993e13799a6fcec4dce8369d89b219b
Maxx <Maxx@Maxx-PC>
2015-10-05 22:20:40 +0200

    Second part of the tp : Abstract IO

--- StackMachine/src/fr/iutvalence/info/m3105/stackmachine/Instruction.java
+++ StackMachine/src/fr/iutvalence/info/m3105/stackmachine/Instruction.java
@@ -3,12 +3,12 @@ package fr.iutvalence.info.m3105.stackmachine;
 public class Instruction {
 
 	private int opCode;
-	private int[] params;
+	private int[] parameters;
 
 	public Instruction(int p_opCode, int[] p_params)
  	{
 		this.opCode = p_opCode;
-		this.params = p_params;
+		this.parameters = p_params;
  	}
 
 	public Object getOpCode() {
